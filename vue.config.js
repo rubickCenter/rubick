7fc60e2ca83e1a154aea36802ead310be2890a68
muwoo <2424880409@qq.com>
2021-12-13 20:17:51 +0800

    :bug: 构建bugfix

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-  transpileDependencies: ["fix-path"],
+  // transpileDependencies: ["fix-path"],
   configureWebpack: {
     resolve: {
       alias: {
