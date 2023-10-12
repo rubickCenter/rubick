90b57d8f0230139a5694a8b576ab11f7170ff634
muwoo <2424880409@qq.com>
2023-10-12 16:01:26 +0800

    :green_heart: fix ci

--- feature/vue.config.js
+++ feature/vue.config.js
@@ -13,10 +13,10 @@ module.exports = {
   },
   outputDir: path.join(__dirname, '../public/feature'),
   publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
-  chainWebpack: (config) => {
-    // 查看打包文件体积大小
-    config
-      .plugin('webpack-bundle-analyzer')
-      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
-  },
+  // chainWebpack: (config) => {
+  //   // 查看打包文件体积大小
+  //   config
+  //     .plugin('webpack-bundle-analyzer')
+  //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
+  // },
 };
